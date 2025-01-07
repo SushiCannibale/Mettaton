0abf938789e405bb16bd5a0b4b9310af15f951c1
SushiCannibale <lpn.marc@gmail.com>
2025-01-08 00:35:05 +0100

    uwu

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ token: str
 with open('secret', 'r') as file:
     token = file.readline()
 
-victims: list[int] = []
-with open('victims', 'r') as file:
-    victims = [ int(line) for line in file.readlines() ]
+# victims: list[int] = []
+# with open('victims', 'r') as file:
+#     victims = [ int(line) for line in file.readlines() ]
 
 intents = discord.Intents.default()
 intents.members = True
 
-client = Mettaton(intents, victims)
+client = Mettaton(intents)
 client.run(token=token)
--- mettaton.py
+++ mettaton.py
@@ -1,25 +1,39 @@
+import requests
 import discord
+from random import randint 
 from discord.ext import tasks
 
-DELAY: float = 10.0
-CHANNEL: int = 1177614879289528396
+# The neko channel
+CHANNEL: int = 1326286426052231229
+URL = 'https://nekos.best/api/v2/neko'
+# TYPES = [ 
+#     'baka', 'bite', 'blush', 'bored', 'cry', 'cuddle', 
+#     'dance', 'facepalm', 'feed', 'handhold', 'handshake', 
+#     'happy', 'highfive', 'hug', 'kick','kiss', 'laugh', 'lurk', 
+#     'nod', 'nom', 'nope', 'pat', 'peck', 'poke', 'pout', 'punch', 
+#     'shoot', 'shrug', 'slap', 'sleep', 'smile', 'smug', 'stare', 
+#     'think', 'thumbsup', 'tickle', 'wave', 'wink', 'yawn', 'yeet' 
+# ]
 
 class Mettaton(discord.Client):
-    def __init__(self, intents: discord.Intents, victims: list[int]):
+    def __init__(self, intents: discord.Intents):
         super().__init__(intents=intents)
-        self.victimsID = victims
-        self.victims: list[discord.Member] = []
 
     async def on_ready(self):
-        for member in self.get_all_members():
-            for id in self.victimsID:
-                if member.id == id:
-                    self.victims.append(member)
-        await self.annoy.start()
-        
+        print("Ready !")
+        await self.send_neko.start()
+        # for member in self.get_all_members():
+        #     for id in self.victimsID:
+        #         if member.id == id:
+        #             self.victims.append(member)
+        # await self.annoy.start()        
 
-    @tasks.loop(seconds=DELAY)
-    async def annoy(self):
-        print("Sent!")
-        for user in self.victims:
-            await self.get_channel(CHANNEL).send(content=user.mention + " =UwU=")
+    @tasks.loop(hours=24)
+    async def send_neko(self):
+        # type = TYPES[randint(0, len(TYPES))]
+        req = requests.get(URL)# + type)
+        print(req)
+        if (req.status_code == 200):
+            data = req.json()
+            print(data)
+            await self.get_channel(CHANNEL).send(content=data['results'][0]['url'])
